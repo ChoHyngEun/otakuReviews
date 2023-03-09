ba418d2eacde900f32abeee7fc3e97a913f5b8d8
javaLearner13 <javalearner13@gmail.com>
2023-03-09 18:02:52 +0900

    edited forPortfolio.sql

--- forPortfolio.sql
+++ forPortfolio.sql
@@ -16,3 +16,30 @@ create table user(
     select * from user;
 --     select * from portfolio.user;
 
+-- -----------------------------------------
+
+-- product table ---------------------------
+
+create table product(
+	product_id int(10) default 0 primary key,
+    product_name varchar(15) default null,
+    product_info varchar(300) default null,
+    product_img varchar(300) default null
+
+);
+
+-- review table ----------------------------
+
+create table review(
+	review_id mediumint not null auto_increment primary key,
+    review_date date,
+    view_count int default 0 not null,
+    likes int default 0 not null
+);
+-- brand table -----------------------------
+
+create table brand(
+	brand_id mediumint not null auto_increment,
+    brand_name varchar(30) default null,
+    primary key(brand_id)
+);
